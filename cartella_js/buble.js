941e6ba2943ca86e885b2eb6ffe679c433035394
SilviaFabbri <laje82@gmail.com>
2021-10-16 18:38:45 +0200

    New Web_site
    I've used Reactjs

--- cartella_js/buble.js
+++
@@ -1,9 +0,0 @@
-let isChrome =  /Chrome/.test(navigator.userAgentData) && /Google Inc/.test(navigator.vendor);
-if (!isChrome){
-    let audio1= document.getElementById("iframeAudio")
-    audio1.remove()
-}
-else{
-    let audio2= document.getElementById("playAudio")
-    audio2.remove()
-}
